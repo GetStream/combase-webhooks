67036814c658f56c60ed62e43be9d9e69096906f
Luke Smetham <luke@arkitek.co.uk>
2021-01-26 12:25:25 +0100

    conditionally send a message directly to dead letter if we have no plugins that can consume it

--- packages/worker/src/combase-ecosystem/createPlugin.js
+++ packages/worker/src/combase-ecosystem/createPlugin.js
@@ -25,6 +25,7 @@ export const createPlugin = plugins =>
 
 		actions(event) {
 			return {
+				log: logger,
 				request: (document, variables) => graphql.request(document, variables, this.authenticateRequest(event)),
 			};
 		}
@@ -32,23 +33,26 @@ export const createPlugin = plugins =>
 		listen = async () => {
 			let i = 0;
 			for await (const [event, ackOrNack] of this.capn.listen(this.triggers)) {
-				console.log(i);
 				try {
 					if (typeof this[event.trigger] === 'function') {
 						await this[event.trigger](event, this.actions(event));
-						i++;
 						ackOrNack();
 					} else if (Array.isArray(this[event.trigger])) {
 						await Promise.all(this[event.trigger].map(fn => fn(event, this.actions(event))));
-						i++;
 						ackOrNack();
 					} else {
-						throw new Error('Event was not consumed.');
+						const canRetry = typeof this[event.trigger] !== 'undefined';
+						const message = canRetry ? 'Something went wrong.' : `No plugins listening for ${event.trigger}`;
+						throw new Object({ canRetry, message });
 					}
 				} catch (error) { 
-					logger.error(error)
-					// TODO: Currently just throws the message to the deferred_retry recovery strategy - we should check the error and conditionally send to dead_letter immediately if applicable.
-					ackOrNack(error, this.capn.engine.broker.config.recovery.deferred_retry);
+					logger.error(error.message)
+					if (error?.canRetry) {
+						// TODO: Currently just throws the message to the deferred_retry recovery strategy - we should check the error and conditionally send to dead_letter immediately if applicable.
+						ackOrNack(error, this.capn.engine.broker.config.recovery.deferred_retry);
+					} else {
+						ackOrNack(error, this.capn.engine.broker.config.recovery.dead_letter);
+					}
 				}
 			}
 		};
