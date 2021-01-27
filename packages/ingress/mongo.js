43136cc73ccb2ae6583ffd0ce7dbb6c302fe469b
Nick Parsons <nparsons08@gmail.com>
2021-01-26 21:39:16 -0600

    Removed auto indexing from connection to reduce burden on DB

--- packages/ingress/mongo.js
+++ packages/ingress/mongo.js
@@ -1,11 +1,8 @@
-import 'dotenv/config';
-import mongoose from 'mongoose';
+import "dotenv/config";
+import mongoose from "mongoose";
 
 export const mongo = await mongoose.connect(process.env.MONGODB_URI, {
-    autoIndex: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    poolSize: 10,
-    useUnifiedTopology: true,
+	useNewUrlParser: true,
+	poolSize: 10,
+	useUnifiedTopology: true,
 });
