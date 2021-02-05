a7297126a4375ed1ee98aa85c1b53e5f436f77ac
Luke Smetham <luke@arkitek.co.uk>
2021-02-05 11:42:12 +0100

    change path to .data for Heroku

--- packages/utils/paths.js
+++ packages/utils/paths.js
@@ -3,8 +3,8 @@ import slash from 'slash';
 
 const createPath = parts => slash(path.join(...parts));
 
-const dataDir = createPath([process.cwd(),'../../', '.data']);
-const manifest = createPath([process.cwd(),'../../', '.data', 'integration-manifest.json']);
+const dataDir = createPath([process.cwd(), '.data']);
+const manifest = createPath([process.cwd(), '.data', 'integration-manifest.json']);
 
 export {
 	dataDir,
