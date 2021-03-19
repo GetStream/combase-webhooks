3d94d720aa5dcd9ff6ac24ce0acd590579e70c66
Luke Smetham <luke@smetham.dev>
2021-03-19 19:40:33 +0100

    update deps

--- packages/worker/src/combase-ecosystem/createPlugin.js
+++ packages/worker/src/combase-ecosystem/createPlugin.js
@@ -8,6 +8,7 @@ export const createPlugin = plugins =>
 			this.triggers = Object.keys(plugins);
 
 			Object.entries(plugins).forEach(([trigger, method]) => {
+				console.log(trigger, method);
 					this[trigger] = method;
 				});
 
@@ -36,15 +37,15 @@ export const createPlugin = plugins =>
 		listen = async () => {
 			for await (const [event, ackOrNack] of this.capn.listen(this.triggers)) {
 				try {
+					console.log(event.trigger, this[event.trigger]);
 					if (typeof this[event.trigger] === 'function') {
 						await this[event.trigger](event, this.actions(event));
-						console.log(event.trigger)
 						ackOrNack();
 					} else if (Array.isArray(this[event.trigger])) {
 						await Promise.all(this[event.trigger].map(fn => fn(event, this.actions(event))));
 						ackOrNack();
 					} else {
-						throw new Error(`${event.trigger} • event was not consumed`)
+						throw new Error(`${event.trigger} event was not consumed`)
 					}
 				} catch (error) { 
 					const canRetry = typeof this[event.trigger] !== 'undefined';
