4cd1b57e7fc5c2258e74ec790e331fe76d1614c1
Luke Smetham <luke@smetham.dev>
2021-05-19 23:58:04 +0100

    fixed property names in hubspot contact creation call

--- packages/plugin-hubspot/src/index.js
+++ packages/plugin-hubspot/src/index.js
@@ -1,28 +1,46 @@
 import Hubspot from 'hubspot';
 
 export const onChannelCreated = async (event, {gql, log, request}) => {
+	// TODO: Get API Key, Portal ID and "combase" property name from integration in mongo
+	// TODO: Also add setup instructions for the above in about.md
 	const hubspot = new Hubspot({ 
 		apiKey: 'API_KEY_HERE'
 	});
 
-	const { user } = data.body;
+	try {
+		const { user } = event.data.body;
+		log.info('hubspot fired');
 
-	await hubspot.contacts.create({
-		properties: [
-		  	{ 
-				property: "name",
-				value: user.name, 
-			},
-		  	{
+		const properties = [
+			{
 				property: "email",
 				value: user.email, 
 			},
-			{ 
-				property: "source",
-				value: "combase", 
-			},
-		]	
-	});
+			{
+				property: "combase",
+				value: true, 
+			}
+		];
 
-	log.info('traacked...');
+		const [firstname, lastname] = user.name.split(' ').map(part => part?.trim?.() || "");
+
+		properties.push({ 
+			property: "firstname",
+			value: firstname, 
+		})
+
+		if (lastname) {
+			properties.push({ 
+				property: "lastname",
+				value: lastname, 
+			})
+		}
+
+		await hubspot.contacts.create({
+			properties	
+		});
+
+	} catch (error) {
+		log.error(error.message);
+	}
 };
