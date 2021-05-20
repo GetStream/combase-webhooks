78f6d545bcf061d78770e2b9069f76d18cfb3c43
Luke Smetham <luke@smetham.dev>
2021-05-20 07:45:24 +0100

    make GA event handler more generic so we can use the same handler for any chat:* event

--- packages/plugin-google-analytics/src/index.js
+++ packages/plugin-google-analytics/src/index.js
@@ -2,29 +2,30 @@ import Analytics from 'analytics'
 import googleAnalytics from '@analytics/google-analytics'
 
 const analytics = Analytics({
-	app: 'awesome-app',
+	app: 'combase',
 	plugins: [
 	  googleAnalytics({
 		trackingId: 'UA-197461081-1', // TODO - move inside the handler and grab tracking id from org integration creds
 	  })
 	]
-  })
+  });
 
-export const onChannelCreated = async (event, {gql, log, request}) => {
-	const { trigger, data } = event;
-	const { user } = data.body;
+export const trackChatEvent = async (event) => {
+	const { data: { body, trigger } } = event;
 	
-	/* Track a custom event */
+	const user = body.user || body.message?.user || body?.channel?.created_by;
+
+	/* Track event in GA with the trigger string as the event name */
 	await analytics.track(trigger, {
 		createdAt: data.body.created_at,
-	})
-	
-	/* Identify a visitor */
-	await analytics.identify(user.id, {
-		combaseId: user.id,
-		name: user.name,
-		email: user.email,
 	});
-
-	log.info('traacked...')
+	
+	/* Identify the user */
+	if (user) {
+		await analytics.identify(user.id, {
+			combaseId: user.id,
+			name: user.name,
+			email: user.email,
+		});
+	}
 };
