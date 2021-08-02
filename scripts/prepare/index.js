be3c1e8197153e201dbc520681e2b8a1ea5878bd
Luke Smetham <luke@smetham.dev>
2021-08-02 21:18:53 +0100

    run install step for plugins in the combase.config.json file

--- scripts/prepare/index.js
+++ scripts/prepare/index.js
@@ -3,11 +3,11 @@ import { dataDir, manifest } from 'utils';
 
 import config from '../../combase.config.json';
 
-import { loadPlugins } from './plugins';
+import { installPlugins, loadPlugins } from './plugins';
 
 try {
 	await fs.ensureDir(dataDir);
-
+	await installPlugins();
 	const plugins = await loadPlugins(config);
 
 	await fs.writeFile(manifest, JSON.stringify(plugins));
